7b390477e0267dfbaa69bf9d99bd00ce4288cf99
Tanzeel Tasleem <tanzeeltasleem648@gmail.com>
2022-03-20 01:24:07 +0500

    feat: add blockchain class

---
+++ Blockchain.ts
@@ -0,0 +1,27 @@
+import { Block } from "./Block";
+
+export class Blockchain {
+    public readonly name:string;
+    public readonly chain: Block[];
+    public readonly numOfBlocks: number = 0 ;
+
+    constructor(name : string ) {
+        this.name = name;
+        this.chain = [this.createGenisisBlock()];
+    }
+
+    private createGenisisBlock() : Block{
+        this.incNumofBlocks()
+        return new Block(0, Date.now().toString() , "Genesis Block" , "0")
+    }
+
+    public addBlock(block:Block){
+        (block.previousHash as string) = this.chain[this.chain.length - 1].previousHash
+        this.chain.push(block);
+        this.incNumofBlocks()
+    }
+
+    private incNumofBlocks(){
+        (this.numOfBlocks as number)++;
+    }
+}
