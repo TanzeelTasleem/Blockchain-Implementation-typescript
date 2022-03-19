abf21b3f649ec047b5b60659df5acd7dbc7c4823
Tanzeel Tasleem <tanzeeltasleem648@gmail.com>
2022-03-20 01:23:10 +0500

    feat: add block class

---
+++ Block.ts
@@ -0,0 +1,24 @@
+import {SHA256} from 'crypto-js';
+
+export class Block {
+    public readonly number:number;
+    public readonly data:any;
+    public readonly timeStamp :string;
+    public readonly hash:string;
+    public readonly previousHash:string;
+
+    constructor(index:number , timeStamp:string , data:any , previousHash: string = "" ){
+        this.number = index;
+        this.data = data; 
+        this.timeStamp = timeStamp;
+        this.previousHash = previousHash
+        this.hash = this.generateBlockHash();
+    }
+
+   /**
+    * Generate Block hash
+    */
+   private generateBlockHash() : string {
+    return SHA256(JSON.stringify({data:this.data,number:this.number,timeStamp:this.timeStamp,previousHash:this.previousHash })).toString()
+   }
+}
