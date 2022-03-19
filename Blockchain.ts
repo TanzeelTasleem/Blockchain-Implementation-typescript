af27efe2f993ea33c6bc9dfa204f04e7b9f522d7
Tanzeel Tasleem <tanzeeltasleem648@gmail.com>
2022-03-20 01:38:36 +0500

    remove "previousHash" error && some changes

--- Blockchain.ts
+++ Blockchain.ts
@@ -1,27 +1,27 @@
 import { Block } from "./Block";
 
 export class Blockchain {
-    public readonly name:string;
+    public readonly name: string;
     public readonly chain: Block[];
-    public readonly numOfBlocks: number = 0 ;
+    public readonly numOfBlocks: number = 0;
 
-    constructor(name : string ) {
+    constructor(name: string) {
         this.name = name;
         this.chain = [this.createGenisisBlock()];
     }
 
-    private createGenisisBlock() : Block{
+    private createGenisisBlock(): Block {
         this.incNumofBlocks()
-        return new Block(0, Date.now().toString() , "Genesis Block" , "0")
+        return new Block(0, Date.now().toString(), "Genesis Block", "0")
     }
 
-    public addBlock(block:Block){
-        (block.previousHash as string) = this.chain[this.chain.length - 1].previousHash
+    public addBlock(block: Block) {
+        (block.previousHash as string) = this.chain[this.chain.length - 1].hash
         this.chain.push(block);
         this.incNumofBlocks()
     }
 
-    private incNumofBlocks(){
+    private incNumofBlocks() {
         (this.numOfBlocks as number)++;
     }
 }
--- index.ts
+++ index.ts
@@ -3,8 +3,12 @@ import { Blockchain } from "./Blockchain";
 
 const blockchain = new Blockchain("MyChain");  //BlockChain initialized 
 
-const b1 = new Block( 1 , Date.now().toString() , {amount : "$50"} ) // block initialized for Blockchain by default Blockchain contains "1" genesis block.
+const b1 = new Block(1, Date.now().toString(), { amount: "$50" }) // block initialized for Blockchain by default Blockchain contains a genesis block.
+
+const b2 = new Block(2, Date.now().toString(), { amount: "$100" }) // block initialized for Blockchain by default Blockchain contains a genesis block.
 
 blockchain.addBlock(b1);  // block added in BlockChain
+blockchain.addBlock(b2);  // block added in BlockChain
 
-console.log(JSON.stringify(blockchain,null , 2)); 
+console.log(JSON.stringify(blockchain, null, 2));
+
